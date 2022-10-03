f433fad729e3acb953a389529e6cacacc999ed0d
Stefan Stoichev <stefan.stoichev@gmail.com>
2022-10-03 09:36:21 +0100

    download command tests done
    closes #65

--- test/download_command.spec.js
+++ test/download_command.spec.js
@@ -29,30 +29,43 @@ describe("Download command", function () {
 
     await cleanup();
 
-    expect(builtScriptResult.code).to.be.equal(0) &&
-      expect(builtScriptResult.stderr.length).to.be.equal(0) &&
-      expect(builtScriptResult.stdout.length).to.be.equal(1) &&
-      expect(builtScript.indexOf("set a = 123")).to.be.greaterThan(-1);
+    expect(downloadResult.code).to.be.equal(0) &&
+      expect(downloadResult.stderr.length).to.be.equal(0) &&
+      expect(downloadResult.stdout.length).to.be.equal(1) &&
+      expect(
+        downloadResult.stdout[0].indexOf("was saved in")
+      ).to.be.greaterThan(-1) &&
+      expect(downloadsFolderContent.length).to.be.equal(1) &&
+      expect(downloadsFolderContent[0].indexOf(".qvf")).to.be.greaterThan(-1);
   });
 
   it("Download command wrong app id", async function () {
-    const { execute, cleanup, writeFile, readFile } =
+    const { execute, cleanup, makeDir, ls, path, readFile } =
       await prepareEnvironment();
 
-    const { code, stdout, stderr } = await execute(
+    const createResult = await execute(
       "node",
-      "./dist/index.js create temp"
+      "./dist/index.js create temp -c configTemplate"
     );
 
-    const builtScriptResult = await execute("node", "./dist/index.js build");
+    await makeDir("./downloads");
+
+    const downloadResult = await execute(
+      "node",
+      `./dist/index.js download ${process.env.QSE_ENV_WRONG_APP_ID} -p ${path}/downloads`,
+      "./temp"
+    );
+
+    const downloadsFolderContent = await ls("./downloads");
 
     await cleanup();
 
-    expect(builtScriptResult.code).to.be.equal(1) &&
-      expect(builtScriptResult.stderr.length).to.be.equal(1) &&
-      expect(builtScriptResult.stdout.length).to.be.equal(0) &&
+    expect(downloadResult.code).to.be.equal(1) &&
+      expect(downloadResult.stderr.length).to.be.equal(1) &&
+      expect(downloadResult.stdout.length).to.be.equal(0) &&
       expect(
-        builtScriptResult.stderr[0].indexOf('✖ "src" folder was not found')
-      ).to.be.greaterThan(-1);
+        downloadResult.stderr[0].indexOf("Request failed with status code 404")
+      ).to.be.greaterThan(-1) &&
+      expect(downloadsFolderContent.length).to.be.equal(0);
   });
 });
