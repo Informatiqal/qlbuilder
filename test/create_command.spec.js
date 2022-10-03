c30655b0fae149c60f510f51e9032cfa666219e9
Stefan Stoichev <stefan.stoichev@gmail.com>
2022-10-03 06:26:43 +0100

    unknown command test

--- test/create_command.spec.js
+++ test/create_command.spec.js
@@ -3,10 +3,20 @@ import { prepareEnvironment } from "@gmrchk/cli-testing-library";
 
 const expect = chai.expect;
 
+// TODO: list of things to test
+// - create empty project
+// - create command without name (false positive)
+// - create command with vscode arg - check vscode folder exists
+// - create command with script template
+// - create command with script template but template dont exists (false positive)
+// - create command with config template
+// - create command with config template but template dont exists (false positive)
+// - create command with all optional arguments - vscode, script anc config templates
+
 describe("Create command", function () {
   this.timeout(30000);
 
-  it("Version", async function () {
+  it("Create empty project", async function () {
     const { execute, ls, cleanup } = await prepareEnvironment();
 
     const { code, stdout, stderr } = await execute(
---
+++ test/unknown_command.spec.js
@@ -0,0 +1,24 @@
+import chai from "chai";
+import { prepareEnvironment } from "@gmrchk/cli-testing-library";
+
+const expect = chai.expect;
+
+describe("Unknown command", function () {
+  this.timeout(30000);
+
+  it("Unknown command", async function () {
+    const { execute, ls, cleanup } = await prepareEnvironment();
+
+    const { code, stdout, stderr } = await execute(
+      "node",
+      "./dist/index.js something temp"
+    );
+
+    await cleanup();
+
+    expect(code).to.be.equal(1) &&
+      expect(stdout.length).to.be.equal(0) &&
+      expect(stderr.length).to.be.equal(2) &&
+      expect(stderr[0]).to.be.equal("Invalid command: something temp");
+  });
+});
