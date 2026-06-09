c6755ba239bc7205bda29de82ab5983de1c6f80e
Stefan Stoichev <stefan.stoichev@gmail.com>
2026-06-09 10:20:45 +0100

    option to write the output to a file

--- src/commands/TablesAndFields.ts
+++ src/commands/TablesAndFields.ts
@@ -1,22 +1,23 @@
+import { writeFileSync } from "fs";
 import { Auth } from "../lib/Auth.js";
 import { Checks } from "../lib/Checks.js";
 import { Config, IConfig } from "../lib/Config.js";
 import { CustomError } from "../lib/CustomError.js";
 import { Engine } from "../lib/Engine.js";
-import { GetScriptOptionValues } from "../types/types.js";
+import { AppDetailsOptions } from "../types/types.js";
 import { Spin } from "../lib/Spinner.js";
 
 export class TablesAndFields {
   private auth: Auth;
   private name: string;
   private environment: IConfig;
-  private options: GetScriptOptionValues;
+  private options: AppDetailsOptions;
   //@ts-ignore
   private spin: Spin;
   script: string = "";
-  constructor(name: string, options?: GetScriptOptionValues) {
+  constructor(name: string, options?: AppDetailsOptions) {
     this.name = name;
-    this.options = options ?? ({} as GetScriptOptionValues);
+    this.options = options ?? ({} as AppDetailsOptions);
     this.spin = new Spin("Checking for syntax errors ...", "hamburger");
 
     const checks = new Checks();
@@ -55,6 +56,7 @@ export class TablesAndFields {
   }
 
   private async getTablesAndFields() {
+    const _this = this;
     const qlik = new Engine(
       this.environment.engineHost,
       this.environment.appId,
@@ -70,32 +72,42 @@ export class TablesAndFields {
       const tablesAndKeys = await doc.getTablesAndKeys({}, {}, 0, true, false);
       await qlik.session.close();
 
-      console.log("");
-      tablesAndKeys.qtr.map(function (t, i) {
+      const displayData: string[] = [];
+
+      tablesAndKeys.qtr.map(function (t) {
         const tags = t.qTableTags.length > 0 ? t.qTableTags.join(", ") : "-";
         const rows = parseInt(t.qNoOfRows).toLocaleString();
-        const tableData = `${t.qName}\n\tRows: ${rows}\n\tTags: ${tags}`;
 
-        console.log(tableData);
-        console.log("\t----------------");
+        displayData.push(t.qName);
+        displayData.push(`\tRows: ${rows}`);
+        displayData.push(`\tTags: ${tags}`);
+        displayData.push("\t----------------");
 
         t.qFields.map(function (f) {
           const tags = f.qTags.length > 0 ? f.qTags.join(", ") : "-";
           const rows = parseInt(f.qnRows).toLocaleString();
           const isKey = f.qKeyType == "NOT_KEY" ? false : true;
 
-          const fieldsData: string[] = [];
-          fieldsData.push(f.qName);
-          if (isKey) fieldsData.push(`Key            : ${f.qKeyType}`);
-          fieldsData.push(`Rows           : ${rows}`);
-          fieldsData.push(`Distinct values: ${f.qnTotalDistinctValues}`);
-          fieldsData.push(`Non null values: ${f.qnNonNulls}`);
-          fieldsData.push(`Tags           : ${tags}`);
-
-          console.log(`\t${fieldsData.join("\n\t\t")}`);
+          displayData.push(`\t${f.qName}`);
+          if (isKey) displayData.push(`\t\tKey            : ${f.qKeyType}`);
+          displayData.push(`\t\tRows           : ${rows}`);
+          displayData.push(`\t\tDistinct values: ${f.qnTotalDistinctValues}`);
+          displayData.push(`\t\tNon null values: ${f.qnNonNulls}`);
+          displayData.push(`\t\tTags           : ${tags}`);
         });
-        console.log("");
+
+        displayData.push("");
       });
+
+      console.log(displayData.join("\n"));
+
+      if (_this.options.output) {
+        try {
+          writeFileSync(_this.options.output, displayData.join("\n"));
+        } catch (e: any) {
+          throw new Error(e.message);
+        }
+      }
     } catch (e: any) {
       await qlik.session.close();
       this.spin.stop();
