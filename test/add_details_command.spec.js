e350006b7b62327e0f1eafe9e86c203940f4865b
Stefan Stoichev <stefan.stoichev@gmail.com>
2026-08-18 12:27:22 +0100

    update build and app details tests

--- test/add_details_command.spec.js
+++ test/add_details_command.spec.js
@@ -1,59 +1,56 @@
-import chai from "chai";
+import { describe, it, expect } from "vitest";
 import { prepareEnvironment } from "@gmrchk/cli-testing-library";
+import { constants } from "./_testing_constants";
 
-const expect = chai.expect;
 describe("App details command", function () {
-  this.timeout(30000);
-
   it("App details command", async function () {
-    const { execute, cleanup, writeFile, readFile } =
+    const { execute, cleanup, writeFile, readFile, path } =
       await prepareEnvironment();
 
     const createResult = await execute(
       "node",
-      "./dist/index.js create temp -c configTemplate"
+      "./dist/index.js create temp -c configTemplate",
     );
 
     const appDetailsResult = await execute(
       "node",
-      `./dist/index.js details ${process.env.QSE_ENV}`,
-      "./temp"
+      `./dist/index.js details local --output app_details.txt`,
+      "./temp",
     );
 
     await cleanup();
 
     expect(appDetailsResult.code).to.be.equal(0) &&
-      expect(appDetailsResult.stderr.length).to.be.equal(0) &&
+      expect(appDetailsResult.stderr.length).to.be.equal(4) &&
       expect(appDetailsResult.stdout.length).to.be.greaterThan(0) &&
       expect(
-        appDetailsResult.stdout[0].indexOf(
-          `${process.env.QSE_ENV_APP_ID_DETAILS}`
-        )
+        appDetailsResult.stdout[1].indexOf(constants.APP_DETAILS_ID),
       ).to.be.greaterThan(-1);
   });
 
   it("App details command with wrong app id", async function () {
-    const { execute, cleanup, writeFile, readFile } =
+    const { execute, cleanup, writeFile, readFile, path } =
       await prepareEnvironment();
 
     const createResult = await execute(
       "node",
-      "./dist/index.js create temp -c configTemplate"
+      "./dist/index.js create temp -c configTemplate",
     );
 
     const appDetailsResult = await execute(
       "node",
-      `./dist/index.js details ${process.env.QSE_ENV_WRONG_APP_ID}`,
-      "./temp"
+      `./dist/index.js details local-wrong-app-id`,
+      "./temp",
     );
 
     await cleanup();
 
+    const appMissingRequest = appDetailsResult.stderr.filter(
+      (e) => e.indexOf("code 404") > -1,
+    );
+
     expect(appDetailsResult.code).to.be.equal(1) &&
-      expect(appDetailsResult.stderr.length).to.be.equal(1) &&
-      expect(appDetailsResult.stdout.length).to.be.equal(0) &&
-      expect(
-        appDetailsResult.stderr[0].indexOf("Socket closed")
-      ).to.be.greaterThan(-1);
+      expect(appMissingRequest.length).to.be.equal(1) &&
+      expect(appDetailsResult.stdout.length).to.be.equal(0);
   });
 });
